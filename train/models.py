a9b3df702e6436133e58b7b1f32a2d94cabf93e0
amateur_dev <49892677+amateur-dev@users.noreply.github.com>
2019-04-23 11:46:06 +0800

    Commit #14 {maintained in different repo earlier}

---
+++ train/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Station(models.Model):
+    city = models.CharField(max_length=64)
+    code = models.CharField(max_length=3)
+
+    def __str__(self):
+        return f"{self.city} ({self.code})"
+
+
+class Train(models.Model):
+    origin = models.ForeignKey(
+        Station, on_delete=models.CASCADE, related_name="departures")
+    destination = models.ForeignKey(
+        Station, on_delete=models.CASCADE, related_name="arrival")
+    duration = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.origin} to {self.destination} in {self.duration} mins"
+
+
+class Passenger(models.Model):
+    fname = models.CharField(max_length=64)
+    lname = models.CharField(max_length=64)
+    trains = models.ManyToManyField(
+        Train, blank=True, related_name="passengers")
+
+    def __str__(self):
+        return f"{self.fname} {self.lname}"
---
+++ train/views.py
@@ -0,0 +1,27 @@
+from django.http import HttpResponse, Http404
+from django.shortcuts import render
+
+from .models import Train
+from .models import Station
+# Create your views here.
+
+
+def index(request):
+    context = {
+        "trains": Train.objects.all(),
+        "stations": Station.objects.all()
+    }
+
+    return render(request, "train/index.html", context)
+
+
+def train_id(request, train_id):
+    try:
+        train = Train.objects.get(id=train_id)
+    except Train.DoesNotExist:
+        raise Http404("Train does not exist")
+    context = {
+        "train": train,
+        "passengers": train.passengers.all()
+    }
+    return render(request, "train/train_id.html", context)
